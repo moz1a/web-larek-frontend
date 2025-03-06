22dea3faf6e65aec61b53518dcff8322b8422fb5
mozia <gusevigor04@gmail.com>
2025-03-06 19:13:02 +0500

    fix types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 //Интерфейс для карточек продуктов каталога
 export interface IProduct {
 	id: string; //уникальный идентификатор
-	category: string; //категория
+	category: ICategory; //категория
 	name: string; //название
 	price: string; //цена
 	image: string; //изображение
@@ -13,24 +13,32 @@ export interface ICatalog {
 	items: IProduct[]; //список продуктов в каталоге
 }
 
+//Типы категорий
+type ICategory =  'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
+
 //Интерфейс для продуктов в корзине
 export interface ICart {
 	items: IProduct[]; //список продуктов в корзине
 	add(product: IProduct): void; //добавить продукт
 	remove(product: IProduct): void; //удалить продукт
 	getTotal(): number; //получить общую стоимость
+  clear(): void; //очищает корзину 
 }
 
 //Интерфейс для деталей заказа
 export interface IOrder {
 	payment: 'Онлайн' | 'При получении'; //способ оплаты
 	address: string; //адрес
+  validateForm(): void; //валидация форм
+  clear(): void; //очищает формы 
 }
 
 //Интерфейс для персональных данных
 export interface IPersonalData {
 	email: string; //электронная почта
 	phone: string; //телефон
+  validateForm(): void; //валидация форм
+  clear(): void; //очищает формы 
 }
 
 //Интерфейс для слушателя событий
